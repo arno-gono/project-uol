7e9d5bf13aaf072e5040d0e47b4f690a774fd000
arno-gono <goyeneche.arnaud@hotmail.fr>
2026-07-26 22:58:09 +0100

    unit test on 1st calibration

--- test/test_calibration_1st_layer.py
+++ test/test_calibration_1st_layer.py
@@ -1,7 +1,33 @@
 import pytest
+import pandas as pd
+from data_calibration.calibration_1st_layer import _get_general_data
 
+def test_get_general_data():
+    df = pd.DataFrame(
+        {
+            "COLUMN_A": [1, 2, 3],
+            "COLUMN_B": ["a", "b", "c"],
+            "COLUMN_C": ["d", "e", "f"],
+            "COLUMN_D": [1.1, 2.2, 3.3],
+        }
+    )
 
-def test_draft():
-    assert 1 * 3 == 3
+    assert _get_general_data(df) == {"nb_entries": 3, "nb_columns": 4}
+
+    # Inserting duplicates
+    df = pd.DataFrame(
+        {
+            "COLUMN_A": [1, 2, 3, 1],
+            "COLUMN_B": [1, 3, 5, 1],
+            "COLUMN_C": [1, 4, 7, 1],
+            "COLUMN_D": [1, 5, 9, 1],
+        }
+    )
+
+    assert _get_general_data(df) == {
+        "nb_entries": 4,
+        "nb_columns": 4,
+        "duplicates_distribution": 0.25
+    }
 
 
